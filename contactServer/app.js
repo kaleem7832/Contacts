51f30749f076c501b090d888501a1276a4e3b781
kaleem7864 <kaleem.nalband@gmail.com>
2023-02-07 15:36:01 +0530

    contactServer is working

--- contactServer/app.js
+++ contactServer/app.js
@@ -23,7 +23,10 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "client/build/")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
